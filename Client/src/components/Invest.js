79a4c8b87f0706bbae2435f7d47bcd9b3413cbd7
Hamza <hamza01961@gmail.com>
2021-08-16 21:34:05 +0500

    fixed api bug

--- Client/src/components/Invest.js
+++ Client/src/components/Invest.js
@@ -94,7 +94,7 @@ function Invest() {
                     console.log(newAmount)
                     const userData = JSON.stringify({'firstName':loginCredentials.data.firstName,'lastName':loginCredentials.data.lastName,'id':loginCredentials.data._id,'amount':newAmount,'coin':coin,'quantity':coinNumbers, 'FollowersId':loginCredentials.data.followersId, 'followers':loginCredentials.data.followers})
                     console.log(userData)
-                    const updateUserAmount = await axios.patch('https://cryptomedia.herokuapp.com/updateAmount',{
+                    const updateUserAmount = await axios.patch('https://cryptomedia.herokuapp.com/api/updateAmount',{
                         userData
                     })
                     if(updateUserAmount.data.success)
